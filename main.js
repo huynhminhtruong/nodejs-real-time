fa859e6d6375ddde630b9bc918ff57f2bf1a81e1
Dave - Truong Huynh Minh <truong.huynh@orientsoftware.net>
2017-08-07 11:37:11 +0700

    Create controller in Angular.

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 var myApp = angular.module('myApp', [])
 
-myApp.controller('PersonController', function($scope){
+var controllers = {}
+
+controllers.PersonController = ($scope) => {
 	$scope.persons = [
 		{ name: 'John', city: 'US' }, 
 		{ name: 'James', city: 'UK' }, 
 		{ name: 'Paul', city: 'AUS' }
 	]
-})
+}
+
+myApp.controller(controllers)
